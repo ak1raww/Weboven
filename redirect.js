098885c9da477cae2811bb6fda4c8f5bb91cf539
yiiky <167021737+ak1raww@users.noreply.github.com>
2026-04-28 17:21:41 +0200

    Updates

--- redirect.js
+++ redirect.js
@@ -2,7 +2,6 @@
   const redirect = sessionStorage.redirect
   delete sessionStorage.redirect
   if (redirect && redirect !== location.pathname) {
-    // This is a clean URL style for BrowserRouter
     history.replaceState(null, null, redirect)
   }
-})();
+})()
